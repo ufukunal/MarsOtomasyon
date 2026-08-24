8d052c69baf7802999253e4488d8fad6f3eec28c
ufukunal <ufuk@ufukunal.com.tr>
2026-08-25 01:43:24 +0300

    feat: load global search stylesheet in shell

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title', 'MarsOtomasyon') · MarsOtomasyon</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/css/search.css', 'resources/js/app.js'])
 </head>
 @php($shell = app(\App\Modules\Core\Shell\ShellContext::class)->state(request()))
 <body class="app-body" data-workspace-title="@yield('title', 'MarsOtomasyon')">
