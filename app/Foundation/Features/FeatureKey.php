03398dd10b5266fb12626a040ee0de806f180bcc
ufukunal <ufuk@ufukunal.com.tr>
2026-08-25 01:25:56 +0300

    feat: define V16.3 application feature keys

--- app/Foundation/Features/FeatureKey.php
+++ app/Foundation/Features/FeatureKey.php
@@ -5,4 +5,16 @@ namespace App\Foundation\Features;
 enum FeatureKey: string
 {
     case Foundation = 'foundation';
+    case Customers = 'customers';
+    case ProductStock = 'product_stock';
+    case Sales = 'sales';
+    case Purchasing = 'purchasing';
+    case Production = 'production';
+    case Treasury = 'treasury';
+    case Instruments = 'instruments';
+    case Returns = 'returns';
+    case Import = 'import';
+    case Commerce = 'commerce';
+    case Communications = 'communications';
+    case Reports = 'reports';
 }
