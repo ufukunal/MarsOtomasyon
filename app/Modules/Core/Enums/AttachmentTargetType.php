36dbbc79cc0c3a3cbc8af5f2621bd4195c2beb31
ufukunal <ufuk@ufukunal.com.tr>
2026-08-25 11:05:05 +0300

    feat(files): add account attachment target

--- app/Modules/Core/Enums/AttachmentTargetType.php
+++ app/Modules/Core/Enums/AttachmentTargetType.php
@@ -5,11 +5,13 @@ namespace App\Modules\Core\Enums;
 enum AttachmentTargetType: string
 {
     case Company = 'company';
+    case Account = 'account';
 
     public function label(): string
     {
         return match ($this) {
             self::Company => 'Firma',
+            self::Account => 'Cari',
         };
     }
 }
