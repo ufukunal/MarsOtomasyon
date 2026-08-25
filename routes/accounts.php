97cbf10eedc9bcb3a9f0637503c7059784dfcca4
ufukunal <ufuk@ufukunal.com.tr>
2026-08-25 11:07:01 +0300

    feat(accounts): route bank notes and files workspace

--- routes/accounts.php
+++ routes/accounts.php
@@ -2,6 +2,7 @@
 
 use App\Modules\Accounts\AccountController;
 use App\Modules\Accounts\AccountProfileController;
+use App\Modules\Accounts\AccountRecordsController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('customers')
@@ -25,6 +26,28 @@ Route::prefix('customers')
             ->whereNumber('account')
             ->middleware('can:accounts.manage')
             ->name('profile.update');
+        Route::get('/{account}/records/edit', [AccountRecordsController::class, 'edit'])
+            ->whereNumber('account')
+            ->middleware('can:accounts.manage')
+            ->name('records.edit');
+        Route::put('/{account}/records', [AccountRecordsController::class, 'update'])
+            ->whereNumber('account')
+            ->middleware('can:accounts.manage')
+            ->name('records.update');
+        Route::post('/{account}/files', [AccountRecordsController::class, 'uploadFile'])
+            ->whereNumber('account')
+            ->middleware('can:accounts.manage')
+            ->name('files.store');
+        Route::get('/{account}/files/{attachment}/download', [AccountRecordsController::class, 'downloadFile'])
+            ->whereNumber('account')
+            ->whereNumber('attachment')
+            ->middleware('can:accounts.view')
+            ->name('files.download');
+        Route::post('/{account}/files/{attachment}/detach', [AccountRecordsController::class, 'detachFile'])
+            ->whereNumber('account')
+            ->whereNumber('attachment')
+            ->middleware('can:accounts.manage')
+            ->name('files.detach');
         Route::get('/{account}', [AccountController::class, 'show'])
             ->whereNumber('account')
             ->middleware('can:accounts.view')
