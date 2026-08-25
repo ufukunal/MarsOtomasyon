85bc9ccf34a82af0b2b28d122194497267db6129
ufukunal <ufuk@ufukunal.com.tr>
2026-08-25 15:42:17 +0300

    M3.2: add product update data

---
+++ app/Modules/Products/Actions/UpdateProductData.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Modules\Products\Actions;
+
+use App\Modules\Products\Enums\ProductStatus;
+
+final readonly class UpdateProductData
+{
+    /** @param list<string> $additionalBarcodes */
+    public function __construct(
+        public string $code,
+        public ProductStatus $status,
+        public string $name,
+        public ?int $categoryId,
+        public int $unitId,
+        public int $taxId,
+        public string $salePriceNet,
+        public string $purchasePriceNet,
+        public ?string $primaryBarcode = null,
+        public array $additionalBarcodes = [],
+    ) {}
+}
