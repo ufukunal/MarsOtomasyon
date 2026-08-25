9ebf71f537dd04b163e8ecd1dc9a1da247505965
ufukunal <ufuk@ufukunal.com.tr>
2026-08-25 18:25:04 +0300

    feat: add canonical stock movement types

---
+++ app/Modules/Inventory/Enums/StockMovementType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Modules\Inventory\Enums;
+
+enum StockMovementType: string
+{
+    case OpeningIn = 'opening_in';
+    case AdjustmentIn = 'adjustment_in';
+    case AdjustmentOut = 'adjustment_out';
+    case TransferIn = 'transfer_in';
+    case TransferOut = 'transfer_out';
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::OpeningIn => 'Açılış Girişi',
+            self::AdjustmentIn => 'Düzeltme Girişi',
+            self::AdjustmentOut => 'Düzeltme Çıkışı',
+            self::TransferIn => 'Transfer Girişi',
+            self::TransferOut => 'Transfer Çıkışı',
+        };
+    }
+
+    public function isInbound(): bool
+    {
+        return match ($this) {
+            self::OpeningIn, self::AdjustmentIn, self::TransferIn => true,
+            self::AdjustmentOut, self::TransferOut => false,
+        };
+    }
+}
