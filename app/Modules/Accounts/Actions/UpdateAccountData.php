f83bf32eeef47e24d96ccb0dc3c50a3aebcf605b
ufukunal <ufuk@ufukunal.com.tr>
2026-08-25 03:26:57 +0300

    feat: add account update data contract

---
+++ app/Modules/Accounts/Actions/UpdateAccountData.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Modules\Accounts\Actions;
+
+use App\Modules\Accounts\Enums\AccountStatus;
+use App\Modules\Accounts\Enums\AccountType;
+use App\Modules\Accounts\Enums\TaxIdentityType;
+
+final readonly class UpdateAccountData
+{
+    public function __construct(
+        public string $code,
+        public AccountType $type,
+        public AccountStatus $status,
+        public string $legalName,
+        public ?string $tradeName,
+        public TaxIdentityType $taxIdentityType,
+        public ?string $taxNumber,
+        public ?string $taxOffice,
+        public string $bookCurrencyCode,
+        public int $dueDays = 0,
+        public string $discountRate = '0',
+        public string $riskLimit = '0',
+    ) {}
+}
