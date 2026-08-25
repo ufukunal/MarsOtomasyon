5f9dc97970b98d0f1cc6832cfa94cbee79582f45
ufukunal <ufuk@ufukunal.com.tr>
2026-08-25 19:24:17 +0300

    feat(inventory): model explicit reversal directions

--- app/Modules/Inventory/Enums/StockMovementType.php
+++ app/Modules/Inventory/Enums/StockMovementType.php
@@ -9,6 +9,8 @@ enum StockMovementType: string
     case AdjustmentOut = 'adjustment_out';
     case TransferIn = 'transfer_in';
     case TransferOut = 'transfer_out';
+    case ReversalIn = 'reversal_in';
+    case ReversalOut = 'reversal_out';
 
     public function label(): string
     {
@@ -18,14 +20,24 @@ enum StockMovementType: string
             self::AdjustmentOut => 'Düzeltme Çıkışı',
             self::TransferIn => 'Transfer Girişi',
             self::TransferOut => 'Transfer Çıkışı',
+            self::ReversalIn => 'Ters Kayıt Girişi',
+            self::ReversalOut => 'Ters Kayıt Çıkışı',
         };
     }
 
     public function isInbound(): bool
     {
         return match ($this) {
-            self::OpeningIn, self::AdjustmentIn, self::TransferIn => true,
-            self::AdjustmentOut, self::TransferOut => false,
+            self::OpeningIn, self::AdjustmentIn, self::TransferIn, self::ReversalIn => true,
+            self::AdjustmentOut, self::TransferOut, self::ReversalOut => false,
+        };
+    }
+
+    public function isReversal(): bool
+    {
+        return match ($this) {
+            self::ReversalIn, self::ReversalOut => true,
+            default => false,
         };
     }
 }
