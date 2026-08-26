7a00f205261b3568d6fe593ff20191019b8530b1
ufukunal <ufuk@ufukunal.com.tr>
2026-08-27 00:03:22 +0300

    Add sales invoice draft input

---
+++ app/Modules/SalesInvoices/Actions/SalesInvoiceDraftData.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Modules\SalesInvoices\Actions;
+
+use App\Modules\SalesInvoices\Enums\SalesInvoiceMode;
+
+final readonly class SalesInvoiceDraftData
+{
+    /** @param list<SalesInvoiceLineData> $lines */
+    public function __construct(
+        public SalesInvoiceMode $mode,
+        public int $sourceBillingAddressId,
+        public string $invoiceDate,
+        public array $lines,
+        public ?int $accountId = null,
+        public ?int $salesOrderId = null,
+        public ?int $dispatchId = null,
+        public ?string $note = null,
+    ) {}
+}
