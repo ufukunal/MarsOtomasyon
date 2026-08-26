b93285cfd1eaf9f27f2246ec3261153d3325d7f8
ufukunal <ufuk@ufukunal.com.tr>
2026-08-27 02:54:41 +0300

    feat(sales-invoices): add lifecycle routes

--- routes/sales-invoices.php
+++ routes/sales-invoices.php
@@ -10,6 +10,14 @@ Route::middleware(['web', 'auth', 'company.context'])
         Route::get('/', [SalesInvoiceController::class, 'index'])->middleware('can:sales_invoices.view')->name('index');
         Route::get('/create', [SalesInvoiceController::class, 'create'])->middleware('can:sales_invoices.manage')->name('create');
         Route::post('/', [SalesInvoiceController::class, 'store'])->middleware('can:sales_invoices.manage')->name('store');
+        Route::post('/{salesInvoice}/finalize', [SalesInvoiceController::class, 'finalize'])
+            ->whereNumber('salesInvoice')
+            ->middleware('can:sales_invoices.manage')
+            ->name('finalize');
+        Route::post('/{salesInvoice}/cancel', [SalesInvoiceController::class, 'cancel'])
+            ->whereNumber('salesInvoice')
+            ->middleware('can:sales_invoices.manage')
+            ->name('cancel');
         Route::get('/{salesInvoice}', [SalesInvoiceController::class, 'show'])
             ->whereNumber('salesInvoice')
             ->middleware('can:sales_invoices.view')
