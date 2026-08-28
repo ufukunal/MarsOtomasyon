0d64e1cfc438abc675d6c1bc1a765d4850a078a4
ufukunal <ufuk@ufukunal.com.tr>
2026-08-28 13:28:17 +0300

    fix: declare treasury payment date model type

--- app/Modules/Treasury/Models/TreasuryPayment.php
+++ app/Modules/Treasury/Models/TreasuryPayment.php
@@ -2,8 +2,10 @@
 
 namespace App\Modules\Treasury\Models;
 
+use Carbon\CarbonImmutable;
 use Illuminate\Database\Eloquent\Model;
 
+/** @property CarbonImmutable $payment_date */
 final class TreasuryPayment extends Model
 {
     protected $guarded = [];
