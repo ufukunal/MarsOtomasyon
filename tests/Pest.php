1c85315ec16b7ccd74edda02fbcdcbf9fa34ca84
ufukunal <ufuk@ufukunal.com.tr>
2026-09-03 01:55:06 +0300

    test: apply fixed clock to sales invoice integrations

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,10 @@
 <?php
 
+use Tests\Support\FixedSalesInvoiceClock;
 use Tests\TestCase;
 
 pest()->extend(TestCase::class)->in('Feature', 'Integration', 'Browser');
+
+pest()->extend(TestCase::class)
+    ->use(FixedSalesInvoiceClock::class)
+    ->in('Integration/SalesInvoices');
