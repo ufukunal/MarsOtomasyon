7f6d83e262793f770029ac7cccbeb7f8e46acb87
ufukunal <ufuk@ufukunal.com.tr>
2026-09-04 06:35:41 +0300

    feat(m23): configure offsite backup storage

--- config/filesystems.php
+++ config/filesystems.php
@@ -18,6 +18,17 @@ return [
             'throw' => false,
             'report' => false,
         ],
+        'mars_backup' => [
+            'driver' => 's3',
+            'key' => env('MARS_BACKUP_S3_ACCESS_KEY_ID'),
+            'secret' => env('MARS_BACKUP_S3_SECRET_ACCESS_KEY'),
+            'region' => env('MARS_BACKUP_S3_REGION', 'us-east-1'),
+            'bucket' => env('MARS_BACKUP_S3_BUCKET'),
+            'endpoint' => env('MARS_BACKUP_S3_ENDPOINT'),
+            'use_path_style_endpoint' => (bool) env('MARS_BACKUP_S3_PATH_STYLE', false),
+            'throw' => true,
+            'report' => false,
+        ],
     ],
     'links' => [
         public_path('storage') => storage_path('app/public'),
