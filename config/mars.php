5b8f026a13e0d4d05c57c5d2a531289b536217d6
ufukunal <ufuk@ufukunal.com.tr>
2026-09-05 15:58:09 +0300

    feat(m26): default label feature off

--- config/mars.php
+++ config/mars.php
@@ -17,6 +17,7 @@ return [
         'automation' => true,
         'operations' => true,
         'reports' => true,
+        'barcode_thermal_labels' => (bool) env('MARS_FEATURE_BARCODE_THERMAL_LABELS', false),
     ],
     'correlation' => [
         'header' => 'X-Correlation-ID',
