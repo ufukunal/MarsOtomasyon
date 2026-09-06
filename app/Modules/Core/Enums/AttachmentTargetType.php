f7eacf99b0cda39d93cd64618ec2cf394638f209
ufukunal <ufuk@ufukunal.com.tr>
2026-09-06 17:26:20 +0300

    feat(m30): add CRM attachment targets

--- app/Modules/Core/Enums/AttachmentTargetType.php
+++ app/Modules/Core/Enums/AttachmentTargetType.php
@@ -6,6 +6,8 @@ enum AttachmentTargetType: string
 {
     case Company = 'company';
     case Account = 'account';
+    case CrmLead = 'crm_lead';
+    case CrmOpportunity = 'crm_opportunity';
     case Product = 'product';
     case ProductFamily = 'product_family';
     case Instrument = 'instrument';
@@ -17,6 +19,8 @@ enum AttachmentTargetType: string
         return match ($this) {
             self::Company => 'Firma',
             self::Account => 'Cari',
+            self::CrmLead => 'CRM Lead',
+            self::CrmOpportunity => 'CRM Fırsat',
             self::Product => 'Ürün',
             self::ProductFamily => 'Ürün ailesi',
             self::Instrument => 'Çek / Senet',
