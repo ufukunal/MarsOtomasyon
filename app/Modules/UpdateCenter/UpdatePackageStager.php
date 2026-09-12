f7bd878215fc304294e7b6a6ec470502053872d1
ufukunal <ufuk@ufukunal.com.tr>
2026-09-12 13:42:52 +0300

    style: satisfy Pint empty constructor rule

--- app/Modules/UpdateCenter/UpdatePackageStager.php
+++ app/Modules/UpdateCenter/UpdatePackageStager.php
@@ -15,8 +15,7 @@ final class UpdatePackageStager
         private readonly Filesystem $files,
         private readonly UpdateUrlPolicy $urlPolicy,
         private readonly UpdateRunStore $runs,
-    )
-    {}
+    ) {}
 
     /**
      * @param array<string, bool|int|string|null> $release
