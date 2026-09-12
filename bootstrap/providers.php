98bfb90e2fc65547081f5877a74665108cafbe28
ufukunal <ufuk@ufukunal.com.tr>
2026-09-12 12:04:11 +0300

    M33: register Update Center deploy-agent provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,8 +2,10 @@
 
 use App\Modules\Core\CoreServiceProvider;
 use App\Providers\AppServiceProvider;
+use App\Providers\UpdateCenterServiceProvider;
 
 return [
     AppServiceProvider::class,
     CoreServiceProvider::class,
+    UpdateCenterServiceProvider::class,
 ];
